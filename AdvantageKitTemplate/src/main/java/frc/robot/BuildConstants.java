699b05b49f47af87c09cab1b2a9c4a4aa8c2d030
CSnair7 <149336161+CSnair7@users.noreply.github.com>
2023-12-16 14:55:04 -0500

    a

---
+++ AdvantageKitTemplate/src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKitTemplate";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 1;
+  public static final String GIT_SHA = "c03f800af24bd245b7e0024b4ae05d7325b50c7b";
+  public static final String GIT_DATE = "2023-12-03 13:25:12 EST";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2023-12-06 20:18:03 EST";
+  public static final long BUILD_UNIX_TIME = 1701911883990L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants() {}
+}
